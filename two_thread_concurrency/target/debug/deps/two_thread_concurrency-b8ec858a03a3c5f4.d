8fa1851b8b54df42241afeafe402c706b08de598
uzakotim <timauzakov@gmail.com>
2024-11-30 15:09:41 +0500

    rust concurrency

---
+++ two_thread_concurrency/target/debug/deps/two_thread_concurrency-b8ec858a03a3c5f4.d
@@ -0,0 +1,5 @@
+/Users/timuruzakov/git/rust_tutorial/two_thread_concurrency/target/debug/deps/two_thread_concurrency-b8ec858a03a3c5f4: src/main.rs
+
+/Users/timuruzakov/git/rust_tutorial/two_thread_concurrency/target/debug/deps/two_thread_concurrency-b8ec858a03a3c5f4.d: src/main.rs
+
+src/main.rs:
